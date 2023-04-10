5bd4ed8208524ee01c2c9ef5700a87f200d3ed49
Fernanda Freitas <idea.fernanda@gmail.com>
2023-04-10 18:28:26 -0300

    reduce method explained

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
   console.log(`unshift - Adiciona um item na primeira posição de um array`)
   const array2 = [4, 5, 6]
   console.log(array2)
-  const newLenghtOfArray = array2.unshift(12)
-  console.log(newLenghtOfArray) // returns the lenght of the new array
+  const newArr2 = array2.unshift(12)
+  console.log(newArr2) // returns the lenght of the new array
   console.log(array2)
 
   // pop - Remove um item na última posição de um array.
@@ -42,28 +42,41 @@ function App() {
   console.log(array6)
   console.log(array5.concat(array6))
 
-  // Every - Itera por todos os elementos do array, verificando uma condição desejada (função) até que false seja devolvido
-  
   // Filter - Cria um array com todos os elementos avaliados com true pela função especificada.
-  console.log(`filter - Cria um array com todos os elementos avaliados com true pela função especificada`)
+  console.log(`filter - Cria um array com todos os elementos que passarem na condição especificada`)
   const array7 = [20, 21, 22]
   console.log(array7)
   const newArr = array7.filter((newArr) => newArr % 2 === 0)
   console.log(newArr)
-  
-  // forEach - Executa uma função específica em cada elemento do array.
+
+  // map - Cria outro array a partir de uma função que contém o critério/condição e devolve os elementos do array que correspondam ao critério.
+  console.log(`map - Cria outro array transformando cada item a partir da iteração em cada item`)
+  const array8 = [23, 24, 25, 26]
+  console.log(array8)
+  const newArr8 = array8.map((newArr) => newArr + 1)
+  console.log(newArr8)
+
+  // reduce - Recebe uma função com os seguintes parâmetros(previousValue, currentValue, index e array). Index e array são opcionais.
+  // podemos usar reduce para devolver um único valor que será somado a um acumulador.
+  console.log(`reduce - Recebe uma função com os seguintes parâmetros(previousValue, currentValue, index e array). Index e array são opcionais. Caso: Somar todos os itens de um array!`)
+  const array9 = [27, 28, 29, 30]
+  console.log(array9)
+  const newArr9 = array9.reduce((prev, current) => prev + current )
+  console.log(newArr9)
   
   // join - Reúne todos os elementos do array em uma string.
+
+  // slice - Devolve um novo array a partir do índice especificado.
+
+  // Every - Itera por todos os elementos do array, verificando uma condição desejada (função) até que false seja devolvido
+  
+  // forEach - Executa uma função específica em cada elemento do array.
   
   // indexOf - Pesquisa o array em busca de elementos específicos e devolve a sua posição.
   
   // lastIndexOf - Devolve a posição do último item do array que corresponda ao critério de pesquisa.
   
-  // map - Cria outro array a partir de uma função que contém o critério/condição e devolve os elementos do array que correspondam ao critério.
-  
   // reverse - Inverte o array, de modo que o último item se torne o primeiro, e vice-versa
-  
-  // slice - Devolve um novo array a partir do índice especificado.
   
   // some - Itera por todos os elementos do array, verificando a condição desejada (função)
   
