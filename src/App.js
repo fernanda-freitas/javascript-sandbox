ce8b78737004ebece6541b2fa6b8f930c7aefcff
Fernanda Freitas <idea.fernanda@gmail.com>
2023-04-10 16:59:25 -0300

    array methods explanations

--- src/App.js
+++ src/App.js
@@ -5,6 +5,21 @@ function App() {
   const array1 = [1, 2, 3, 4, 5]
   const array2 = [6, 7, 8, 9, 10]
 
+  // Concat - Junta vários arrays e devolve uma cópia dos arrays concatenados.
+  // Every - Itera por todos os elementos do array, verificando uma condição desejada (função) até que false seja devolvido
+  // Filter - Cria um array com todos os elementos avaliados com true pela função especificada.
+  // forEach - Executa uma função específica em cada elemento do array.
+  // join - Reúne todos os elementos do array em uma string.
+  // indexOf - Pesquisa o array em busca de elementos específicos e devolve a sua posição.
+  // lastIndexOf - Devolve a posição do último item do array que corresponda ao critério de pesquisa.
+  // map - Cria outro array a partir de uma função que contém o critério/condição e devolve os elementos do array que correspondam ao critério.
+  // reverse - Inverte o array, de modo que o último item se torne o primeiro, e vice-versa
+  // slice - Devolve um novo array a partir do índice especificado.
+  // some - Itera por todos os elementos do array, verificando a condição desejada (função)
+  // sort - Organiza o array em ordem alfabética ou de acordo com a função específicada.
+  // toString - Devolve o array na forma de uma string.
+  // valueOf - É semelhante ao método toString e devolve o array na forma de uma string.
+
   const concat = () => {
     
   }
