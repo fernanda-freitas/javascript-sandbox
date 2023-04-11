e9e2a5cb215f79f21203ec887f2d0fead23d6e1b
Fernanda Freitas <idea.fernanda@gmail.com>
2023-04-11 06:29:26 -0300

    array exercise component added

--- src/ArrayExercise.js
+++ src/ArrayExercise.js
@@ -1,11 +1,8 @@
 export default function ArrayExercise() {
 
     // função solution(A);
-    // 1.
     // Dado um array A de N inteiros, retorna o menor inteiro positivo (maior que 0)
-    // 2.
     // que não ocorre em A.
-
     // Por exemplo, dada A = [1, 3, 6, 4, 1, 2], a função deve retornar 5.
     // Dado A = [1, 2, 3], a função deve retornar 4.
     // Dado A = [−1, −3], a função deve retornar 1.
@@ -19,19 +16,14 @@ export default function ArrayExercise() {
         // Garante que só há inteiros positivos no array
         const positiveA = sortedA.filter((num) => num > 0)
         console.log(positiveA)
-        // Remove possíveis duplicados
-        const removedDuplicates = positiveA.map((item, index) => {
-            // return `${item} ${index}`
-            if (item[index] !== item[index + 1]) {
-                return item
-            } else return 1
-        })
-        console.log(removedDuplicates)
+        // TODO: Remove possíveis duplicados
+
+        // TODO: Usa o método Reduce para retornar o menor valor não existente
     }   
     const A = [-2, -1, 0, 1, 3, 6, 4, 1, 2]
     console.log(solution(A))
 
     return (
-        <h4>Array exercise</h4>
+        <h4>Array exercises</h4>
     )
 }
